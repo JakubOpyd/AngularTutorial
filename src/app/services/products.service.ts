01b1d5b29cca41113cb19d95206f1f12996794f1
JakubOpyd <jakubopyd@gmail.com>
2024-08-13 18:11:30 +0200

    Creating interfaces for "Products", "Product" and "PaginationParams"

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+
+  
+
 }
