4c52f775230e7141ca82d34e0ef0e693bfcf92eb
JakubOpyd <jakubopyd@gmail.com>
2024-08-21 17:00:15 +0200

    Added UI for REST API

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, viewChild } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
-import { PaginatorModule } from 'primeng/paginator';
+import { Paginator, PaginatorModule } from 'primeng/paginator';
+import { EditPopupComponent } from '../components/edit-popup/edit-popup.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule, PaginatorModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule,EditPopupComponent,ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
+  @ViewChild('paginator') paginator: Paginator | undefined;
 
   products: Product[] = [];
 
   totalRecords: number = 0;
   rows: number = 5;
+
+  displayEditPopup: boolean = false;
+  displayAddPopup: boolean = false;
+
+  toggleEditPopup(product: Product){
+    this.selectedProduct = product;
+    this.displayEditPopup = true;
+  }
+
+  toggleAddPopup(){
+    this.displayAddPopup = true;
+  }
+
+  toggleDeletePopup(product: Product){
+    if (!product.id) {
+      return;
+    }
+    this.deleteProduct(product.id);
+  }
+
+  selectedProduct: Product = {
+    id:0,
+    name:'',
+    image:'',
+    price:'',
+    rating:0,
+  }
+
+  onConfirmEdit(product: Product){
+    if (!this.selectedProduct.id) {
+      return;
+    }
+    
+    this.editProduct(product,this.selectedProduct.id);
+    this.displayAddPopup= false;
+  }
+
+  onConfirmAdd(product: Product){
+    this.addProduct(product);
+    console.log(product, 'Output');
+  }
+
+
+  resetPaginator(){
+    this.paginator?.changePage(0);
+  }
 
   onPageChange(event: any) {
     this.fetchProducts(event.page, event.rows);
@@ -45,6 +94,7 @@ export class HomeComponent {
         next: (data) => {
           console.log(data);
           this.fetchProducts(0, this.rows);
+          this.resetPaginator();
         },
         error: (error) => {
           console.log(error);
@@ -59,6 +109,7 @@ export class HomeComponent {
         next: (data) => {
           console.log(data);
           this.fetchProducts(0, this.rows);
+          this.resetPaginator();
         },
         error: (error) => {
           console.log(error);
@@ -73,6 +124,7 @@ export class HomeComponent {
         next: (data) => {
           console.log(data);
           this.fetchProducts(0, this.rows);
+          this.resetPaginator();
         },
         error: (error) => {
           console.log(error);
